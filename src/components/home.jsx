4ad56c941385d9e4788d6adcdf537a7d8074cd3b
Cristian Bucur <cristian.bucur@dedalus.eu>
2022-10-20 15:01:32 +0100

    added nav files

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./title";
+import Header from "./header";
+export default function home() {
+  return (
+    <div>
+      home
+      <Header />
+      <Title />
+    </div>
+  );
+}
